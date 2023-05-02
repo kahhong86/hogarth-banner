97fa90e0c8e03f09e40b3c4ceee492aafe758cb9
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2023-05-02 23:21:13 +0800

    Added replay button

--- js/script.js
+++ js/script.js
@@ -1,39 +1,43 @@
+const tl = gsap.timeline();
+
 const Animation = () => {
-    const tl = gsap.timeline();
     tl.to('.bg',{
-        width:300,height:250,opacity:1,duration:2,
-    });
-    gsap.to('.caption',{
-        opacity:0.8,duration:2
+        width:300,height:250,opacity:1,duration:2,x:0,y:0
     })
-    gsap.to('.logo',{
+    .to('.caption',{
+        opacity:0.8,duration:2
+    }, 0)
+    .to('.logo',{
         opacity:1,duration:2
-    });
-    gsap.to('.bg',{
-        x:-25,y:-25,duration:12,scale:1.2
-    });
-    tl.to('.text1',{
-        opacity:0,duration:2
-    });
-    tl.to('.text2',{
-        opacity:1,duration:2
-    });
-    tl.to('.text2',{
-        opacity:0,duration:2
-    });
-    tl.to('.text3',{
-        opacity:1,duration:2
-    });
-    tl.to('.text3',{
+    }, 0)
+    .to('.text1',{
         opacity:0,duration:2,
-    });
-    tl.to('.text4',{
+    }, 3)
+    .to('.text2',{
+        opacity:1,duration:2
+    },"-=1")
+    .to('.text2',{
+        opacity:0,duration:2
+    })
+    .to('.text3',{
+        opacity:1,duration:2
+    },"-=1")
+    .to('.text3',{
+        opacity:0,duration:2,
+    })
+    .to('.text4',{
         opacity:1,duration:2,
-    });
+    },"-=1")
+    .to('button',{
+        display:"block",opacity:1
+    })
+    .to('.bg',{
+        x:-25,y:-25,duration:12,scale:1.2
+    }, 0)
 }
 
 const handleClick = () => {
-    Animation();
-}
+    tl.restart(); 
+}  
 
 Animation();
