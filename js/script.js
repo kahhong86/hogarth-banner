8552104f45b3d9d2481897f2024462b5d802f227
Cheong Kah Hong <kahhong_cheong@git.savantdegrees.com>
2023-05-03 16:03:14 +0800

    Added 3 more secs animation with 4 panel for ending

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const tl = gsap.timeline();
 const bg_tl = gsap.timeline();
+const tl_finale = gsap.timeline();
 
 const Animation = () => {
     tl.to('.caption',{
@@ -25,10 +26,7 @@ const Animation = () => {
     })
     .to('.text4',{
         opacity:1,duration:1,
-    },"-=1")
-    .to('button',{
-        display:"block",opacity:1
-    })
+    },"-=1");
 
     //Background Timeline
     bg_tl.set('.bg',{
@@ -62,12 +60,45 @@ const Animation = () => {
         x:0,y:0,scale:1
     },{
         x:-10,y:-10,duration:3,scale:1.2,
+    });
+
+    tl_finale.to('.content',{
+        delay:12,opacity:0,duration:0.5
     })
+    .to('.finale',{
+        opacity:1,display:"block",
+    },"<")
+    .to('.bg1',{
+        duration:0.5,opacity:1,
+    })
+    .to('.bg2',{
+        duration:0.5,opacity:1,backgroundImage:"url(images/home-panel-02-edit.jpg)",width:225,height:250
+    })
+    .to('.bg1',{
+        width:75,duration:0.5,backgroundPosition:"center center"
+    },"<")
+    .to('.bg3',{
+        duration:0.5,opacity:1,backgroundImage:"url(images/home-panel-03-m.jpg)",width:150,height:250
+    })
+    .to('.bg2',{
+        width:75,duration:0.5,backgroundPosition:"57% center"
+    },"<")
+    .to('.bg4',{
+        duration:0.5,opacity:1,backgroundImage:"url(images/home-panel-04.jpg)",width:75,height:250,backgroundPosition:"center center"
+    })
+    .to('.bg3',{
+        width:75,duration:0.5,backgroundPosition:"center center"
+    },"<")
+    .to('button',{
+        opacity:1,duration:0.5,display:"block"
+    })
+    ;
 }
 
 const handleClick = () => {
     tl.restart();
     bg_tl.restart();
+    tl_finale.restart();
 }  
 
 Animation();
